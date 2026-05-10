9006167b291c6bc6cc65eac6681db072f5b072b6
Isivara Rajapakse <isivara2021@gmail.com>
2026-05-10 13:41:43 +0530

    Delete .next-build directory

--- .next-build/server/chunks/ssr/app_layout_tsx_13yf0ls._.js
+++
@@ -1,3 +0,0 @@
-module.exports=[33290,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"en",children:(0,b.jsxs)("body",{className:"relative min-h-dvh bg-black text-white antialiased",children:[(0,b.jsx)("div",{className:"fixed inset-0 -z-20 aurora-bg"}),(0,b.jsx)("div",{className:"fixed inset-0 -z-10 grid-bg opacity-70 [mask-image:radial-gradient(60%_60%_at_50%_20%,black,transparent_70%)]"}),(0,b.jsx)("div",{className:"fixed inset-0 -z-10 noise"}),a]})})},"metadata",0,{title:"Isivara | Portfolio",description:"Full-Stack Developer Portfolio"}])},70864,a=>{a.n(a.i(33290))}];
-
-//# sourceMappingURL=app_layout_tsx_13yf0ls._.js.map
